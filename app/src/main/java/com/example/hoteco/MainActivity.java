72a52d0b438544ab7715c139ae0497413a8206e9
Malith Akalanka Senadheera <71032773+Malitthh@users.noreply.github.com>
2021-09-26 19:45:00 +0530

    Add files via upload

--- app/src/main/java/com/example/hoteco/MainActivity.java
+++ app/src/main/java/com/example/hoteco/MainActivity.java
@@ -1,117 +1,343 @@
-package com.example.hoteco;
+package com.example.expensestracker;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.Menu;
-
-import com.example.hoteco.home.AddNewHotelActivity;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private NavigationView navigationView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.MutableDateTime;
+import org.joda.time.Weeks;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView weekSpendingTv, budgetTv, todaySpendingTv,remainingBudgetTv,monthSpendingTv;
+
+    private CardView budgetCardView, todayCardView, homeBtn, weekCardView, monthCardView, historyCardView;
+
+    // private ImageView weekBtnImageView, todayBtnImageView, monthBtnImageView, analyticsImageView; /*budgetBtnImageView ;*/
+
+    private CardView analyticsCardView;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference budgetRef, expensesRef, personalRef;
+    private String onlineUserID = "";
+
+    private int totalAmountMonth = 0;
+    private int totalAmountBudget = 0;
+    private  int totalAmountBudgetB = 0;
+    private  int totalAmountBudgetC = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitleTextColor(Color.WHITE);
+
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Expenses Tracker");
 
-        ButterKnife.bind(this);
+        budgetTv = findViewById(R.id.budgetTv);
+        todaySpendingTv = findViewById(R.id.todaySpendingTv);
+        remainingBudgetTv = findViewById(R.id.remainingBudgetTv);
+        monthSpendingTv = findViewById(R.id.monthSpendingTv);
+        weekSpendingTv = findViewById(R.id.weekSpendingTv);
 
-        navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        toggle.setDrawerIndicatorEnabled(true);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        mAuth = FirebaseAuth.getInstance();
+        onlineUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        budgetRef = FirebaseDatabase.getInstance().getReference("budget").child(onlineUserID);
+        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        personalRef = FirebaseDatabase.getInstance().getReference("personal").child(onlineUserID);
 
 
+        homeBtn = findViewById(R.id.homeBtn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        todayCardView = findViewById(R.id.todayCardView);
+
+        analyticsCardView = findViewById(R.id.analyticsCardView);
+
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TodaySpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        analyticsCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChooseAnalyticActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        monthCardView = findViewById(R.id.monthCardView);
+        monthCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weekCardView = findViewById(R.id.weekCardView);
+        weekCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        historyCardView = findViewById(R.id.historyCardView);
+        historyCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        budgetRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists() && snapshot.getChildrenCount()>0){
+                    for (DataSnapshot ds :  snapshot.getChildren()){
+                        Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                        Object total = map.get("amount");
+                        int pTotal = Integer.parseInt(String.valueOf(total));
+                        totalAmountBudgetB+=pTotal;
+                    }
+                    totalAmountBudgetC = totalAmountBudgetB;
+                    personalRef.child("budget").setValue(totalAmountBudgetC);
+                }else {
+                    personalRef.child("budget").setValue(0);
+                    Toast.makeText(MainActivity.this, "Please Set a BUDGET ", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        getBudgetAmount();
+        getTodaySpentAmount();
+        getWeekSpentAmount();
+        getMonthSpentAmount();
+        getSavings();
+    }
+
+    private void getBudgetAmount() {
+        budgetRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists() && snapshot.getChildrenCount()>0){
+                    for (DataSnapshot ds :  snapshot.getChildren()){
+                        Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                        Object total = map.get("amount");
+                        int pTotal = Integer.parseInt(String.valueOf(total));
+                        totalAmountBudget+=pTotal;
+                        budgetTv.setText("$ "+String.valueOf(totalAmountBudget));
+                    }
+                }else {
+                    totalAmountBudget=0;
+                    budgetTv.setText("$ "+String.valueOf(0));
+
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void getTodaySpentAmount(){
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        String date = dateFormat.format(cal.getTime());
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        Query query = reference.orderByChild("date").equalTo(date);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                int totalAmount = 0;
+                for (DataSnapshot ds :  dataSnapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+                    todaySpendingTv.setText("$ "+ totalAmount);
+                }
+                personalRef.child("today").setValue(totalAmount);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getMonthSpentAmount(){
+        MutableDateTime epoch = new MutableDateTime();
+        epoch.setDate(0); //Set to Epoch time
+        DateTime now = new DateTime();
+        Months months = Months.monthsBetween(epoch, now);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        Query query = reference.orderByChild("month").equalTo(months.getMonths());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalAmount = 0;
+                for (DataSnapshot ds :  dataSnapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+                    monthSpendingTv.setText("$ "+ totalAmount);
+
+                }
+                personalRef.child("month").setValue(totalAmount);
+                totalAmountMonth = totalAmount;
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getWeekSpentAmount(){
+        MutableDateTime epoch = new MutableDateTime();
+        epoch.setDate(0); //Set to Epoch time
+        DateTime now = new DateTime();
+        Weeks weeks = Weeks.weeksBetween(epoch, now);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserID);
+        Query query = reference.orderByChild("week").equalTo(weeks.getWeeks());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalAmount = 0;
+                for (DataSnapshot ds :  dataSnapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+                    weekSpendingTv.setText("$ "+ totalAmount);
+                }
+                personalRef.child("week").setValue(totalAmount);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getSavings(){
+        personalRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull final DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+
+                    int budget;
+                    if (snapshot.hasChild("budget")) {
+                        budget = Integer.parseInt(snapshot.child("budget").getValue().toString());
+                    } else {
+                        budget = 0;
+                    }
+                    int monthSpending;
+                    if (snapshot.hasChild("month")) {
+                        monthSpending = Integer.parseInt(Objects.requireNonNull(snapshot.child("month").getValue().toString()));
+                    } else {
+                        monthSpending = 0;
+                    }
+
+                    int savings = budget - monthSpending;
+                    remainingBudgetTv.setText("$ " + savings);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            Intent intent = new Intent(this, MainActivity.class);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.account){
+            Intent intent  = new Intent(MainActivity.this, AccountActivity.class);
             startActivity(intent);
-        } else if (id == R.id.nav_advertisements) {
-                Intent intent = new Intent(this, AdvertisementsActivity.class);
-                startActivity(intent);
-        } else if (id==R.id.nav_feedback){
-            Intent intent = new Intent(this, FeedbackActivity.class) ;
-            startActivity(intent);
-        } else if (id == R.id.nav_expense_tracker) {
-            Intent intent = new Intent(this, ExpenseTrackerActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_profile) {
-            Intent intent = new Intent(this, ProfileActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_logout) {
-
         }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @OnClick(R.id.btn_addNewHotel)
-    void goToAddNewHotel() {
-        Intent intent = new Intent(this, AddNewHotelActivity.class);
-        startActivity(intent);
-    }
-
 }
-
--- app/src/main/java/com/example/hoteco/RegistrationActivity.java
+++ app/src/main/java/com/example/hoteco/RegistrationActivity.java
@@ -1,16 +1,36 @@
-package com.example.hoteco;
+package com.example.expensestracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button registerBtn;
+    private TextView registerQn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        getSupportActionBar().hide();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.registerBtn);
+        registerQn = findViewById(R.id.registerQn);
+
+        registerQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (packageContext: RegistrationActivity.this, LoginActivity.class);
+                startActivity(Intent);
+            }
+        });
+
     }
 }
